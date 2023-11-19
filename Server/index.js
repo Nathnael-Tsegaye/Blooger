86c26536b46bad9e6591b7301ca6d31dcaf0f5ca
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2023-11-19 19:15:22 +0000

    Database setup completed

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+require("dotenv").config();
+const connectDb = require("./config/db");
 
 const app = express();
+
+connectDb();
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
